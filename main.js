0d2e7c1615d7be8e2dc6ccd5741a615f5a87cd55
Seganation <xdniklaus@yahoo.com>
2024-02-01 00:26:58 +0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-// main.js
-
 function copyToClipboard() {
-  const textToCopy = "Mamosa HAck kray 400$.exe"; // Replace with the actual email address
+  const textToCopy = "Trojan-Horse.exe" ; // Replace with the actual email address
 
   const tempInput = document.createElement("input");
   tempInput.value = textToCopy;
@@ -17,4 +15,28 @@ function copyToClipboard() {
   setTimeout(function() {
     copyContainer.classList.remove("clicked");
   }, 2000);
+}
+
+
+
+let isDarkMode = false;
+
+function darkMode() {
+  isDarkMode = !isDarkMode;
+
+  // Toggle between dark and light mode icons
+  const darkModeIcon = document.getElementById("darkModeIcon");
+  const lightModeIcon = document.getElementById("lightModeIcon");
+
+  if (isDarkMode) {
+    document.body.style.backgroundColor = "#142B50";
+    darkModeIcon.style.display = "none";
+    lightModeIcon.style.display = "inline";
+    // Add your dark mode styles or functionality here
+  } else {
+    document.body.style.backgroundColor = "whitesmoke";
+    darkModeIcon.style.display = "inline";
+    lightModeIcon.style.display = "none";
+    // Add your light mode styles or functionality here
+  }
 }
